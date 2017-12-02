454c23220d8c695758d2fdf0a045adc01bc0672b
maxwen <max.weninger@gmail.com>
2017-12-02 18:26:40 -0500

    Custom button light [2/2]
    *DU Edits*
    
    - Adapt to our custom seekbar
    - Wrap preferences/seekbar with a category / update strings
    - Add check to remove preferences depending on bool (config_button_brightness_support)
    
    @jhenrique09 edits: Revert old commits and cleanup code
    
    @Hlcpereira edits: Update to solve conflicts if commit PixelExperience/packages_apps_CustomSettings@faa53cb is in commit history
    
    Change-Id: I90531723f8751a92766e96f5409d979a2f5bd524

--- src/com/toxyc/hazard/fragments/ButtonSettings.java
+++ src/com/toxyc/hazard/fragments/ButtonSettings.java
@@ -29,6 +29,8 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.ListPreference;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.os.PowerManager;
+import android.os.ServiceManager;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -37,6 +39,7 @@ import com.android.settings.Utils;
 
 import com.toxyc.hazard.preference.ActionFragment;
 import com.toxyc.hazard.preference.ActionPreference;
+import com.toxyc.hazard.preference.CustomSeekBarPreference;
 
 import com.android.internal.util.hwkeys.ActionConstants;
 import com.android.internal.util.hwkeys.ActionUtils;
@@ -47,6 +50,11 @@ public class ButtonSettings extends ActionFragment implements
 
     // Keys
     private static final String HWKEY_ENABLE = "hardware_keys_enable";
+
+    // Brightness
+    private static final String KEY_BUTTON_MANUAL_BRIGHTNESS_NEW = "button_manual_brightness_new";
+    private static final String KEY_BUTTON_TIMEOUT = "button_timeout";
+    private static final String KEY_BUTON_BACKLIGHT_OPTIONS = "button_backlight_options_category";
 
     // category keys
     private static final String CATEGORY_HWKEY = "hardware_keys";
@@ -70,6 +78,10 @@ public class ButtonSettings extends ActionFragment implements
     private ContentResolver resolver;
 
     private SwitchPreference mHwKeyEnable;
+
+    private CustomSeekBarPreference mButtonTimoutBar;
+    private CustomSeekBarPreference mManualButtonBrightness;
+    private PreferenceCategory mButtonBackLightCategory;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -189,6 +201,34 @@ public class ButtonSettings extends ActionFragment implements
             }
         }
 
+        ContentResolver resolver = getContentResolver();
+
+        mManualButtonBrightness = (CustomSeekBarPreference) findPreference(
+                KEY_BUTTON_MANUAL_BRIGHTNESS_NEW);
+        final int customButtonBrightness = getResources().getInteger(
+                com.android.internal.R.integer.config_button_brightness_default);
+        final int currentBrightness = Settings.System.getInt(resolver,
+                Settings.System.CUSTOM_BUTTON_BRIGHTNESS, customButtonBrightness);
+        PowerManager pm = (PowerManager)getActivity().getSystemService(Context.POWER_SERVICE);
+        mManualButtonBrightness.setMax(pm.getMaximumScreenBrightnessSetting());
+        mManualButtonBrightness.setValue(currentBrightness);
+        mManualButtonBrightness.setOnPreferenceChangeListener(this);
+
+        mButtonTimoutBar = (CustomSeekBarPreference) findPreference(KEY_BUTTON_TIMEOUT);
+        int currentTimeout = Settings.System.getInt(resolver,
+                Settings.System.BUTTON_BACKLIGHT_TIMEOUT, 0);
+        mButtonTimoutBar.setValue(currentTimeout);
+        mButtonTimoutBar.setOnPreferenceChangeListener(this);
+
+        final boolean enableBacklightOptions = getResources().getBoolean(
+                com.android.internal.R.bool.config_button_brightness_support);
+
+        mButtonBackLightCategory = (PreferenceCategory) findPreference(KEY_BUTON_BACKLIGHT_OPTIONS);
+
+        if (!enableBacklightOptions) {
+            prefScreen.removePreference(mButtonBackLightCategory);
+        }
+
         // let super know we can load ActionPreferences
         onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
 
@@ -208,7 +248,16 @@ public class ButtonSettings extends ActionFragment implements
             Settings.Secure.putInt(getContentResolver(), Settings.Secure.HARDWARE_KEYS_DISABLE,
                     value ? 0 : 1);
             setActionPreferencesEnabled(value);
-            return true;
+        } else if (preference == mButtonTimoutBar) {
+            int buttonTimeout = (Integer) newValue;
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.BUTTON_BACKLIGHT_TIMEOUT, buttonTimeout);
+        } else if (preference == mManualButtonBrightness) {
+            int buttonBrightness = (Integer) newValue;
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.CUSTOM_BUTTON_BRIGHTNESS, buttonBrightness);
+        } else {
+            return false;
         }
         return true;
     }
